7f0df0fc0f43608fa16db81aa70c151e54319ab9
Steven <stevenhuyn@gmail.com>
2023-07-20 10:06:51 -0700

    Use railway backend

--- src/utility/Api.ts
+++ src/utility/Api.ts
@@ -2,7 +2,7 @@ import { Scenario } from "./Scenario";
 
 const BACKEND_BASE_URL =
   window.location.host === "plakait.com"
-    ? new URL("https://plakait-backend.onrender.com/")
+    ? new URL("https://plakait-backend-production.up.railway.app/")
     : new URL("http://localhost:8000/");
 
 export type BotMessage = {
